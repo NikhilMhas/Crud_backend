41d931685485a070531fe01108c137fb51d6f8e2
Nikhil Mhaske <105561400+NikhilMhas@users.noreply.github.com>
2024-12-13 14:30:34 +0530

    Update postgres.js

--- postgres/postgres.js
+++ postgres/postgres.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
 import { createUserModel } from '../model/userSchema.js';
 
-const sequelize = new Sequelize('crud', 'postgres', 'Nik@1234', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  logging: false,
+});
 
 let userModel=null;
 
